f73a3a778113ccc9e6b34d87cf4633b246edf376
Max Estrella <maxestrella565@gmail.com>
2024-06-14 14:52:36 +0100

    Toggle discontinued cases function

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ function toggleRareCase() {
     document.querySelector('.rare-case-drop').classList.toggle('visible')
 }
 
+// DISCONTINUED CASE DROP POOL TOGGLE
+function toggleDiscontinuedCase() {
+    document.querySelector('.discontinued-cases').classList.toggle('visible')
+}
