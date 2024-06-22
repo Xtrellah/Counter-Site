7975b472150ee401bd0048ddb0e99c717545b6ff
Max Estrella <maxestrella565@gmail.com>
2024-06-22 18:47:33 +0100

    Addition of news page and blog.json

--- script.js
+++ script.js
@@ -7,3 +7,23 @@ function toggleRareCase() {
 function toggleDiscontinuedCase() {
     document.querySelector('.discontinued-cases').classList.toggle('visible')
 }
+
+// NEWS/BLOG
+fetch('blog.json').then(function (res) {
+    return res.json()
+}).then(function(data) {
+    
+    const target = document.querySelector('.target')
+    
+    data.blog.forEach(function (blog){
+            console.log(blog.name)
+            target.innerHTML += `
+            <div>
+                <img src="${blog.image}" />
+                <h2>${blog.title}</h2>
+                <p>${blog.desc}</p>
+                <a href="${blog.link}">Check it out on GitHub!</a>
+            </div>
+        `
+    })
+})
