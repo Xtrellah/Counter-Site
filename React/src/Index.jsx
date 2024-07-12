fc615601787245e854eace909b4b3abd747d0016
Max Estrella <maxestrella565@gmail.com>
2024-07-12 18:02:17 +0100

    Router installed and setup

---
+++ React/src/Index.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+
+
+export default function Index() {
+
+  return (
+    <BrowserRouter>
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+
+    </BrowserRouter>
+  )
+}
