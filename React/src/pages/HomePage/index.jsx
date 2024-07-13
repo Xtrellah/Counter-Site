2e7aca9e06c6813af1776d568a6097d8da81fe41
Max Estrella <maxestrella565@gmail.com>
2024-07-13 13:18:57 +0100

    Some progress made

--- React/src/pages/HomePage/index.jsx
+++ React/src/pages/HomePage/index.jsx
@@ -1,9 +1,12 @@
+import CaseDropPool from "../../components/CaseDropPool";
 
 export default function HomePage() {
 
     return (
         <div>
-
+            <h1 class="audiowide-regular">Counter-Site</h1>
+            <p>Some general info about Counter-Strike 2</p>
+            <CaseDropPool />
         </div>
     )
 }
