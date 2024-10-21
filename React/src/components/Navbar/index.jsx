b89b4ce8eccc30761402e1ab3c23743a8d000a37
Max Estrella <maxestrella565@gmail.com>
2024-10-21 13:01:09 +0100

    Navbar update

--- React/src/components/Navbar/index.jsx
+++ React/src/components/Navbar/index.jsx
@@ -1,13 +1,38 @@
+import './style.css';
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 
 export default function Navbar() {
 
+    const location = useLocation();
+
+    useEffect(() => {
+        const links = document.querySelectorAll('a');
+        let currentPath = location.pathname.split("/").pop();
+
+        if (currentPath === '') {
+            currentPath = '';
+        }
+
+        links.forEach(link => {
+            const hrefPath = link.getAttribute('href').split("/").pop();
+            console.log('LOOK HERE' + hrefPath)
+
+            if (hrefPath === currentPath) {
+                link.classList.add('active');
+            } else {
+                link.classList.remove('active');
+            }
+        });
+    }, [location]);
+
     return (
-        <ul className="navbar">
+        <ul className="navbar" id="navbar">
             <li><a href="/">Home</a></li>
             <li><a href="/News">News</a></li>
             <li><a href="/commands">Commands</a></li>
             <li><a href="/Placeholder">Login</a></li>
-            <li><a href="/feed">Feed</a></li>
+            <li><a href="/Feed">Feed</a></li>
         </ul>
-    )
+    );
 }
--- React/src/main.jsx
+++ React/src/main.jsx
@@ -4,7 +4,7 @@ import Index from './Index.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <Index />
-  </React.StrictMode>,
+  // </React.StrictMode>,
 )
